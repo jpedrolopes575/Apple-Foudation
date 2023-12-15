648f24aa4ceb3207ce05fa7b0f1e4b30a324a526
João Pedro Lopz <125674695+jpedrolopes575@users.noreply.github.com>
2023-12-15 13:10:52 -0300

    Add files via upload

---
+++ Handebol/handebol 2/handebol 2/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  handebol 2
+//
+//  Created by user on 15/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View{
+   var body: some View{
+       
+       ZStack{
+           
+           Color.blue
+               .edgesIgnoringSafeArea(.all)
+           VStack(spacing: 35){
+               Text ("HANDEBOL")
+                   .font(.system(size:44))
+                   .foregroundColor(.white)
+                   .bold()
+               HStack{
+                   Image("Carinha")
+                       .resizable()
+                       .frame(width:150, height:150)
+                       .cornerRadius(10)
+                   VStack {
+                       ZStack {
+                           Image("Laranja")
+                               .resizable()
+                               .frame(width:150, height:150)
+                               .cornerRadius(10)
+                       }
+                       ZStack{
+                           Image("Verde")
+                               .resizable()
+                               .frame(width:150, height:150)
+                               .cornerRadius(10)
+                       }
+                   }
+                   
+                           
+                       
+                   }
+                   Text ("Vamo pra SEC ?")
+                   .font(.system(size: 30))
+                       .foregroundColor(.white)
+                       .bold()
+                       
+                   Button{
+                       
+                   } label: {
+                       Text ("vamo ou bora?")
+                           .font(.system(size: 30))
+                           .foregroundColor(.black)
+                           .padding(.vertical,30)
+                           .padding(.horizontal,30)
+                           .background(Color.white)
+                           .cornerRadius(50)
+                           
+               }
+           }
+       }
+       
+   }
+}
---
+++ Handebol/handebol 2/handebol 2/handebol_2App.swift
@@ -0,0 +1,17 @@
+//
+//  handebol_2App.swift
+//  handebol 2
+//
+//  Created by user on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct handebol_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
